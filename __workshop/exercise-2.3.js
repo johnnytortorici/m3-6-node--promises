d42073fe56b16427d26ddfe969cdfc34742d4ac1
Johnny Tortorici <johnny.tortorici@gmail.com>
2020-07-11 17:39:52 -0400

    Exercise 2.3

--- __workshop/exercise-2.3.js
+++ __workshop/exercise-2.3.js
@@ -5,3 +5,25 @@
 const request = require('request-promise');
 
 // get the code you wrote in 2.2 and paste it here...
+const greeting = (langCode) => {
+    return request(`https://journeyedu.herokuapp.com/hello/${langCode}`) // 1
+        .then((response) => JSON.parse(response))
+        .then((parsedResponse) => {
+            return parsedResponse.data; // 2
+        })
+        .catch((err) => {
+            // if err.error is true (contains info), convert that info
+            // from string back to object (via parse) and return obj
+            return err.error ? JSON.parse(err.error) : err;
+        });
+};
+
+// Testing
+greeting('fr').then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
+
+// 3
+greeting('it').then((result) => console.log(result)); // { lang: 'Italian', code: 'IT', text: 'Salve' }
+greeting('ja').then((result) => console.log(result)); // { lang: 'Japanese', code: 'JA', text: 'Konnichiwa' }
+
+// 4 - Test error
+greeting('jp').then((result) => console.log(result)); // { lang: 'Japanese', code: 'JA', text: 'Konnichiwa' }
