55bfab1b3a1528e225e707bf16c45d33518d7a0b
Johnny Tortorici <johnny.tortorici@gmail.com>
2020-07-12 12:44:48 -0400

    Exercise 3.2

--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -2,17 +2,35 @@
 // ---------------------------------
 
 const opencage = require('opencage-api-client');
-require('dotenv').config();
+const env = require('dotenv').config();
 
 const getPositionFromAddress = (address) => {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: env.parsed.OPENCAGE_API_KEY,
+    q: address,
   };
 
-  // return something...
+  return opencage.geocode(requestObj)
+    .then((response) => { 
+      let geometry = response.results[0].geometry;
+      return geometry;
+    })
+    .catch((err) => {
+      return err.message;
+    });
 };
 
+// Concordia
 getPositionFromAddress(
   '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8'
-).then((response) => console.log(response));
+).then((response) => console.log(response)); // { lat: 45.497118, lng: -73.579044 }
+
+// Eiffel Tower
+getPositionFromAddress(
+  '5 Avenue Anatole France, 75007 Paris, France'
+).then((response) => console.log(response)); // { lat: 48.8582602, lng: 2.2944991 }
+
+// Colosseum
+getPositionFromAddress(
+  'Piazza del Colosseo, 1, 00184 Roma RM, Italy'
+).then((response) => console.log(response)); // { lat: 48.8582602, lng: 2.2944991 }
